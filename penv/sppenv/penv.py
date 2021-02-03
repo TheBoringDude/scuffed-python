4464fb7aa9ef5f2614928d8d2b0881a6f2a8018c
TheBoringDude <iamcoderx@gmail.com>
2021-02-03 20:59:07 +0800

    0.1.1

--- penv/sppenv/penv.py
+++ penv/sppenv/penv.py
@@ -11,4 +11,8 @@ def load_env(overwrite: bool = True):
             # check if there is equals sign in each environment variable set
             if "=" in os_var:
                 var, value = os_var.replace("\n", "").split("=")
-                os.environ[var] = value
+
+                # replace variables spaces with _ and uppercase it
+                #  >> strip, replace, uppercase
+                # trim white spaces in the value
+                os.environ[var.strip().replace(" ", "_").upper()] = value.strip()
--- penv/tests/test_penv.py
+++ penv/tests/test_penv.py
@@ -22,3 +22,19 @@ def test_env_load():
     assert os.getenv("TEST") == "123"
     assert os.environ["HELLO"] == "WORLD"
     assert os.getenv("NOT_SET_ENV") == None
+
+
+# small caps variables set in .env
+def test_env_smallcaps():
+    load_env()
+
+    assert os.getenv("smallvarsenv") == None
+    assert os.getenv("SMALLVARSENV") == "env"
+
+
+# variables with spaces
+def test_env_spaces():
+    load_env()
+
+    assert os.getenv("env with spaces ") == None
+    assert os.getenv("ENV_WITH_SPACES") == "value"
